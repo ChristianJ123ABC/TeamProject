f30dd8604ec74446b5aed91e35e44892b655c081
Prakash012b <148553683+Prakash012b@users.noreply.github.com>
2025-02-12 17:24:06 +0000

    Update CanAndDash.sql

--- CanAndDash.sql
+++ CanAndDash.sql
@@ -8,7 +8,7 @@ CREATE TABLE Users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     role ENUM('customer', 'driver', 'food_owner') NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
