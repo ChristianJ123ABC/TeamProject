8df5793a79f86f355512d62d9e75a88b7ff00e17
abznci2004 <abzali15427@gmail.com>
2025-02-17 14:59:03 +0000

    updtaed
    update

--- templates/Basket.jsx
+++
@@ -1,9 +0,0 @@
-export default function Basket() {
-    return (
-      <div className="page-container">
-        <h1>Basket Page</h1>
-        <p>View and manage your discounted food orders.</p>
-      </div>
-    );
-  }
-  
--- templates/Recycle.jsx
+++
@@ -1,8 +0,0 @@
-export default function Recycle() {
-    return (
-      <div className="page-container">
-        <h1>Recycle Page</h1>
-        <p>Learn how to recycle and earn rewards!</p>
-      </div>
-    );
-  }
